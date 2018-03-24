c2158938d5b3cdf58741f9c9d169b197e04d097e
Abhi Nath <abhi.rs@icloud.com>
2018-03-25 00:10:52 +1100

    Initial Commit

---
+++ indices/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  indices
+//
+//  Created by Abhilash on 25/3/18.
+//  Copyright © 2018 abhi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
